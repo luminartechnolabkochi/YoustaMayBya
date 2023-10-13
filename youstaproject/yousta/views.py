e06a3ec2fd0faddd1b5b299b26ebd39d3cd16713
sajay <sajaykannan10@gmail.com>
2023-10-13 12:05:18 +0530

    login post

--- youstaproject/yousta/views.py
+++ youstaproject/yousta/views.py
@@ -1,6 +1,6 @@
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.views.generic import CreateView,FormView
 from yousta.forms import RegistrationForm,LoginForm
@@ -8,12 +8,14 @@ from yousta.models import User
 from django.urls import reverse_lazy
 from django.contrib import messages
 
+from django.contrib.auth import authenticate,login,logout
+
 class SignUpView(CreateView):
 
     template_name="yousta/register.html"
     form_class=RegistrationForm
     model=User
-    success_url=reverse_lazy("signup")
+    success_url=reverse_lazy("signin")
 
     def form_valid(self,form):
         messages.success(self.request,"account created")
@@ -26,7 +28,23 @@ class SignUpView(CreateView):
 class SignInView(FormView):
     template_name="yousta/login.html"
     form_class=LoginForm
-    
+
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+
+        if form.is_valid():
+            uname=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            usr=authenticate(request,username=uname,password=pwd)
+            if usr:
+                login(request,usr)
+                messages.success(request,"login successfully")
+                return redirect("signin")
+            else:
+                messages.error(request,"invalid creadential")
+                return render(request,self.template_name,{"form":form})
+
+
 
 
 
