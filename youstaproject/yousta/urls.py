675e4c756f380cf1b7c941dafe40525b7c5fe4ed
sajay <sajaykannan10@gmail.com>
2023-10-13 11:54:40 +0530

    url mappaed

--- youstaproject/yousta/urls.py
+++ youstaproject/yousta/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from yousta.views import SignUpView
+from yousta.views import SignUpView,SignInView
 
 
 
 urlpatterns=[
 
     path("register/",SignUpView.as_view(),name="signup"),
-    
+    path("",SignInView.as_view(),name="signin")
+
 
 
 
