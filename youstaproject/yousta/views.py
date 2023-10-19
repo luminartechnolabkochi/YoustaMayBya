a450e5300a8391573d8e9b55bccc95fa070a1a44
sajay <sajaykannan10@gmail.com>
2023-10-19 11:32:30 +0530

    bslogin

--- youstaproject/yousta/views.py
+++ youstaproject/yousta/views.py
@@ -6,9 +6,36 @@ from django.views.generic import CreateView,FormView,ListView,UpdateView,DetailV
 from django.urls import reverse_lazy,reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from django.utils.decorators import method_decorator
 
 from yousta.forms import RegistrationForm,LoginForm,CategoryCreateForm,ClothAddForm,ClothVarientForm,OfferForm
 from yousta.models import User,Category,Cloths,ClothVarients,Offers
+
+
+
+def signin_required(fn):
+
+    def wrapper(request,*args,**kwargs):
+        if not  request.user.is_authenticated:
+            messages.error(request,"invalid session!!")
+            return redirect("signin")
+        else:
+            return fn(request,*args,**kwargs)
+    return wrapper
+
+def is_admin(fn):
+
+    def wrapper(request,*args,**kwargs):
+        if  not request.user.is_superuser:
+            messages.error(request,"permission denied for current user ! !")
+            return redirect("signin")
+        else:
+            return fn(request,*args,**kwargs)
+    return wrapper
+
+
+decs=[signin_required,is_admin]
+
 
 
 
@@ -47,6 +74,7 @@ class SignInView(FormView):
                 return render(request,self.template_name,{"form":form})
 
 
+@method_decorator(decs,name="dispatch")
 class CategoryCreateView(CreateView,ListView):
 
     template_name="yousta/category_add.html"
@@ -65,6 +93,8 @@ class CategoryCreateView(CreateView,ListView):
         return Category.objects.filter(is_active=True)
 
 
+@signin_required
+@is_admin
 def remove_category(request,*args,**kwargs):
     id=kwargs.get("pk")
     Category.objects.filter(id=id).update(is_active=False)
@@ -72,7 +102,7 @@ def remove_category(request,*args,**kwargs):
     return redirect("add-category")
 
 
-
+@method_decorator(decs,name="dispatch")
 class ClothCreateView(CreateView):
 
     template_name="yousta/cloth_add.html"
@@ -86,14 +116,14 @@ class ClothCreateView(CreateView):
     def form_invalid(self, form):
         messages.error(self.request,"cloth adding failed ")
         return super().form_invalid(form)
-    
 
+@method_decorator(decs,name="dispatch")   
 class ClothListView(ListView):
     template_name="yousta/cloth_list.html"
     model=Cloths
     context_object_name="cloths"
 
-
+@method_decorator(decs,name="dispatch")
 class ClothUpdateView(UpdateView):
     template_name="yousta/cloth_edit.html"
     model=Cloths
@@ -107,7 +137,8 @@ class ClothUpdateView(UpdateView):
         messages.error(self.request,"cloth updating failed")
         return super().form_invalid(form)
 
-
+@signin_required
+@is_admin
 def remove_clothview(request,*args,**kwargs):
     id=kwargs.get("pk")
     Cloths.objects.filter(id=id).delete()
@@ -116,7 +147,7 @@ def remove_clothview(request,*args,**kwargs):
 # localhost:8000/clothvarient/1/add
 # localhost:8000/cloths/1/varients/add
 
-
+@method_decorator(decs,name="dispatch")
 class ClothVarientCreateView(CreateView):
     template_name="yousta/clothvarient_add.html"
     form_class=ClothVarientForm
@@ -131,14 +162,14 @@ class ClothVarientCreateView(CreateView):
     
         return super().form_valid(form)
     
-
+@method_decorator(decs,name="dispatch")
 class ClothDetailView(DetailView):
     template_name="yousta/cloth_detail.html"
     model=Cloths
     context_object_name="cloth"
 
 
-
+@method_decorator(decs,name="dispatch")
 class ClothVarientUpdateView(UpdateView):
     template_name="yousta/varient_edit.html"
     form_class=ClothVarientForm
@@ -158,7 +189,8 @@ class ClothVarientUpdateView(UpdateView):
         cloth_id=cloth_varient_object.cloth.id
         return reverse("cloth-detail",kwargs={"pk":cloth_id})
 
-
+@signin_required
+@is_admin
 def remove_varient(request,*args,**kwargs):
     id=kwargs.get("pk")
     ClothVarients.objects.filter(id=id).delete()
@@ -166,7 +198,7 @@ def remove_varient(request,*args,**kwargs):
 
 
 # localhost:8000/varients/<int:pk>/offers/add
-
+@method_decorator(decs,name="dispatch")
 class OfferCreateView(CreateView):
     template_name="yousta/offer_add.html"
     form_class=OfferForm
@@ -194,7 +226,8 @@ class OfferCreateView(CreateView):
 
        return reverse("cloth-detail",kwargs={"pk":cloth_id})
 
-
+@signin_required
+@is_admin
 def offer_delete_view(request,*args,**kwargs):
     id=kwargs.get("pk")
     offer_object=Offers.objects.get(id=id)
@@ -202,7 +235,7 @@ def offer_delete_view(request,*args,**kwargs):
     offer_object.delete()
     return redirect("cloth-detail",pk=cloth_id)
 
-
+@signin_required
 def sign_out_view(request,*args,**kwargs):
     logout(request)
     return redirect("signin")
